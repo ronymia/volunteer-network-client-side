d8afb62f7823bc046742058b4d6041109a432eae
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-09-26 00:33:11 +0600

    navigate

--- src/layouts/UserAuth.jsx
+++ src/layouts/UserAuth.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import logo from "../assets/logos/Group 1329.png";
 
 export default function UserAuth() {
+     const navigate = useNavigate();
+
      return (
           <div className="flex flex-col items-center justify-center gap-y-5 pt-10 bg-[#F8FAFC]">
                <img src={logo} alt=""
-                    className="h-16"
+                    className="h-16 cursor-pointer"
+                    onClick={() => navigate("/")}
                />
                <Outlet />
           </div>
